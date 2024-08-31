1046272c7de79cf08e5dca5729548a14e4413ccd
unknown <omotosoabiola9525@gmail.com>
2024-08-31 15:29:16 +0100

    urls

--- account/urls.py
+++ account/urls.py
@@ -3,4 +3,7 @@ from account import views
 
 urlpatterns = [
     path('account/register/', views.register_user, name='register-user'),
+    path('account/confirm/', views.confirm_email, name='confirm-email'),
+    path('account/token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    
 ]
